2db957241bdf5ae66d30811bbae99bcd2b9758bf
khushiiagrawal <khushisaritaagrawal@gmail.com>
2025-01-03 02:05:15 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { FooterWithSitemap } from "./components/footer";
-import { NavbarWithMegaMenu } from "./components/navbar";
+
+import LandingPage from "./landing-page/components/landingpage";
+
 import "./index.css";
 
 import { ThemeProvider } from "@material-tailwind/react";
@@ -11,8 +12,10 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <NavbarWithMegaMenu /> 
-      <FooterWithSitemap /> 
+      
+      <LandingPage/>
+     
+ 
     </ThemeProvider>
   </React.StrictMode>
 );
