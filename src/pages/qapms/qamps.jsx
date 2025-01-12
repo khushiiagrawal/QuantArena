bd1d38c91b8e5c9e4d01fa14d967e2eedc40e599
khushiiagrawal <khushisaritaagrawal@gmail.com>
2025-01-13 02:35:16 +0530

    qapms page

--- src/pages/qapms/qamps.jsx
+++ src/pages/qapms/qamps.jsx
@@ -3,7 +3,7 @@ import Req from "./request";
 export default function QAPMS() {
     return (
       <div>
-        <div>Quality Assurance Performance Management System</div>
+        
         <div>
           <Req/>
         </div>
