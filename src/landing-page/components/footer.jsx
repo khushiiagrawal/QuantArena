64d2d376a6125baf3d2b3a754698814fee9ad756
khushiiagrawal <khushisaritaagrawal@gmail.com>
2025-01-13 22:03:18 +0530

    header and footer

--- src/landing-page/components/footer.jsx
+++ src/landing-page/components/footer.jsx
@@ -15,7 +15,7 @@ export default function Footer() {
   return (
     <footer className="bg-black text-white py-12">
       {/* Top Section */}
-      <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-8 items-center border-b border-gray-600 pb-8">
+      <div className="max-w-7xl mx-auto container px-4 md:px-6 grid grid-cols-1 md:grid-cols-3 gap-8 items-center border-b border-gray-600 pb-8">
         {/* Phone */}
         <div className="flex items-center space-x-4">
           <div className="w-12 h-12 rounded-full bg-[#9E6AED] flex items-center justify-center">
@@ -37,23 +37,19 @@ export default function Footer() {
           <div className="w-12 h-12 rounded-full bg-[#9E6AED] flex items-center justify-center">
             <img src={Location} alt="Location" className="h-6 w-6 text-white" />
           </div>
-
-          <span className="text-lg">Office no. A-15, Sr. no. 90, <br /> Majestique Memories, NIBM <br /> Annex,Mohammadwadi,<br/> Pune - 411060 (MH)</span>
+          <span className="text-lg">Office no. A-15, Sr. no. 90, <br /> Majestique Memories, NIBM <br /> Annex, Mohammadwadi,<br /> Pune - 411060 (MH)</span>
         </div>
       </div>
 
       {/* Bottom Section */}
-      <div className="max-w-auto mx-auto mt-10 grid grid-cols-1 md:grid-cols-5">
+      <div className="max-w-7xl container mx-auto mt-10 px-4 grid grid-cols-1 md:grid-cols-5 gap-10">
         {/* Logo and Subscribe */}
-        
+        <div className="col-span-1 md:col-span-1 flex justify-center md:justify-start">
           <h1 className="text-xl font-montserrat px-20 py-2 font-bold">LOGO</h1>
-
-          {/* <img src="/path/to/logo.png" alt="Logo" className="h-12 w-auto mx-auto" /> */}
-
-      
+        </div>
 
         {/* Links */}
-        <div className="col-span-1 max-w-auto mx-auto container md:col-span-4 grid grid-cols-2 md:grid-cols-5 gap-12">
+        <div className="col-span-1 md:col-span-4 grid grid-cols-2 md:grid-cols-5 gap-8">
           <div>
             <h2 className="font-bold mb-4 underline text-white">Pages</h2>
             <ul className="space-y-2">
@@ -61,8 +57,8 @@ export default function Footer() {
               <li><a href="/" onClick={handleServicesClick} className="text-white">Services</a></li>
               {showServicesDropdown && (
                 <ul className="space-y-2 pl-4">
-                  <li><a href="/service1" className="text-white">Service 1</a></li>
-                  <li><a href="/service2" className="text-white">Service 2</a></li>
+                  <li><a href="/service1" className="text-white">Analytics</a></li>
+                  <li><a href="/service2" className="text-white">Risk Management</a></li>
                 </ul>
               )}
               <li><a href="/qamps" className="text-white">QAPMS</a></li>
@@ -89,7 +85,6 @@ export default function Footer() {
               <li className="text-white"><a href="/" className="text-white">Policy Review</a></li>
               <li className="text-white"><a href="/" className="text-white">Enterprise Risk Framework</a></li>
               <li className="text-white"><a href="/" className="text-white">Risk Review</a></li>
-
             </ul>
           </div>
 
@@ -99,7 +94,6 @@ export default function Footer() {
               <li><a className="text-white" href="/">Terms & Condition</a></li>
               <li> <a className="text-white" href="/">Privacy Policy</a></li>
               <li> <a className="text-white" href="/">Legal Disclaimer</a></li>
-
             </ul>
           </div>
 
@@ -108,14 +102,13 @@ export default function Footer() {
             <ul className="space-y-2">
               <li><a className="text-white" href="/">Instagram</a></li>
               <li><a className="text-white" href="/">Facebook</a></li>
-              <li><a className="text-white " href="/">LinkedIn</a></li>
-
+              <li><a className="text-white" href="/">LinkedIn</a></li>
             </ul>
           </div>
         </div>
-        <div className="text-center mt-4 text-gray-400">
-          © {currentYear} All Rights Reserved
-        </div>
+      </div>
+      <div className="text-center mt-8 text-gray-400">
+        © {currentYear} All Rights Reserved
       </div>
     </footer>
   );
--- src/landing-page/components/header.jsx
+++ src/landing-page/components/header.jsx
@@ -1,11 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
   const [openDropdownIndex, setOpenDropdownIndex] = useState(null);
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const [headerBg, setHeaderBg] = useState("bg-white bg-opacity-80");
+  const [headerBg, setHeaderBg] = useState("bg-white bg-opacity-70");
   const location = useLocation();
+  const dropdownRef = useRef(null);
 
   const navItems = [
     { label: "Home", link: "/home" },
@@ -32,9 +33,9 @@ export default function Header() {
         const sectionBottom = section.offsetTop + section.offsetHeight;
 
         if (window.scrollY <= sectionBottom) {
-          setHeaderBg("bg-white bg-opacity-90");
+          setHeaderBg("bg-white bg-opacity-70");
         } else {
-          setHeaderBg("bg-black bg-opacity-60");
+          setHeaderBg("bg-black bg-opacity-70");
         }
       }
     };
@@ -54,7 +55,7 @@ export default function Header() {
       setHeaderBg("bg-white bg-opacity-90");
     }
     if (location.pathname === "/contact-us") {
-      setHeaderBg("bg-black bg-opacity-60");
+      setHeaderBg("bg-black bg-opacity-70");
     }
   }, [location.pathname]);
 
@@ -73,65 +74,80 @@ export default function Header() {
 
   const textColorClass = headerBg.includes("bg-black") ? "text-white" : "text-black";
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpenDropdownIndex(null);
+      }
+    };
+
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div id="Header">
-      <header className={`fixed w-full flex items-center justify-between px-8 md:px-16 py-3 md:py-5 ${headerBg} font-montserrat shadow-md transition-colors duration-300 ${location.pathname === "/career" ? "z-50" : ""}`}>
+      <header className={`fixed w-full flex items-center justify-between px-8 md:px-16 py-3 md:py-5 ${headerBg} font-montserrat shadow-md transition-colors duration-300 z-10`}>
         <div className={`text-2xl md:text-2xl ${textColorClass}`}>Logo</div>
-        <div className="flex-1 flex justify-center">
-          <nav className={`md:flex items-center md:text-xl gap-4 md:gap-10 text-lg ${isMobileMenuOpen ? "block z-50" : "hidden"} md:block`}>
-            {navItems.map((item, index) => (
-              <div key={index} className="relative">
-                <Link
-                  to={item.link}
-                  className={`${textColorClass} hover:text-gray-600 hover:underline transition-colors flex items-center`}
-                  onClick={() => {
-                    if (item.hasDropdown) {
-                      handleDropdownToggle(index);
-                    } else {
-                      setOpenDropdownIndex(null);
-                    }
-                    setIsMobileMenuOpen(false);
-                  }}
-                >
-                  {item.label}
-                  {item.hasDropdown && (
-                    <svg
-                      className={`ml-1 md:ml-3 w-4 h-4 transform ${openDropdownIndex === index ? "rotate-180" : "rotate-0"} transition-transform ${textColorClass}`}
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M19 9l-7 7-7-7"
-                      ></path>
-                    </svg>
-                  )}
-                </Link>
-                {item.hasDropdown && openDropdownIndex === index && (
-                  <ul className={`absolute top-full left-0 mt-2 w-48 ${headerBg.includes("bg-white") ? "bg-black text-white" : "bg-white text-black"} shadow-lg z-50`}>
-                    <li className="px-4 py-2 hover:bg-gray-300 cursor-pointer">
-                      <Link to="/services/service1">Service 1</Link>
-                    </li>
-                    <li className="px-4 py-2 hover:bg-gray-300 cursor-pointer">
-                      <Link to="/services/service2">Service 2</Link>
-                    </li>
-                  </ul>
+        <nav className={`flex-1 flex items-center justify-center md:text-xl gap-4 md:gap-10 text-lg ${isMobileMenuOpen ? "block z-50" : "hidden"} md:flex`}>
+          {navItems.map((item, index) => (
+            <div key={index} className="relative" ref={item.hasDropdown ? dropdownRef : null}>
+              <Link
+                to={item.link}
+                className={`${textColorClass} hover:text-gray-600 hover:underline transition-colors flex items-center`}
+                onClick={() => {
+                  if (item.hasDropdown) {
+                    handleDropdownToggle(index);
+                  } else {
+                    setOpenDropdownIndex(null);
+                  }
+                  setIsMobileMenuOpen(false);
+                }}
+              >
+                {item.label}
+                {item.hasDropdown && (
+                  <svg
+                    className={`ml-1 md:ml-3 w-4 h-4 transform ${openDropdownIndex === index ? "rotate-180" : "rotate-0"} transition-transform ${textColorClass}`}
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M19 9l-7 7-7-7"
+                    ></path>
+                  </svg>
                 )}
-              </div>
-            ))}
-            <Link
-              to="/contact-us"
-              className={`block md:hidden bg-[#9E6AED] text-sm text-center text-white px-1 py-1 rounded hover:bg-purple-200 transition-colors`}
-              onClick={() => setIsMobileMenuOpen(false)}
-            >
-              Contact Us
-            </Link>
-          </nav>
-        </div>
+              </Link>
+              {item.hasDropdown && openDropdownIndex === index && (
+                <ul className={`absolute top-full rounded left-0 mt-2 w-60 bg-white text-black shadow-lg z-50`}>
+                  <Link to="/analytics" onClick={() => setOpenDropdownIndex(null)}>
+                    <li className="px-2 py-4 text-center hover:bg-gray-300 cursor-pointer">
+                      <span className="text-[#9E6AED]">Analytics</span>
+                    </li>
+                  </Link>
+                  <Link to="/risk-management" onClick={() => setOpenDropdownIndex(null)}>
+                    <li className="px-2 py-4 text-center hover:bg-gray-300 cursor-pointer">
+                      <span className="text-[#9E6AED]">Risk Management</span>
+                    </li>
+                  </Link>
+                </ul>
+              )}
+            </div>
+          ))}
+          <Link
+            to="/contact-us"
+            className={`block md:hidden bg-[#9E6AED] text-sm text-center text-white px-1 py-1 rounded hover:bg-purple-200 transition-colors`}
+            onClick={() => setIsMobileMenuOpen(false)}
+          >
+            Contact Us
+          </Link>
+        </nav>
         <div className="hidden md:block">
           <Link
             to="/contact-us"
