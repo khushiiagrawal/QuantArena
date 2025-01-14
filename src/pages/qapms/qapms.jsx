9fe1cccc96ddbce55d9a733302c038046b5b487a
khushiiagrawal <khushisaritaagrawal@gmail.com>
2025-01-14 23:58:01 +0530

    request

---
+++ src/pages/qapms/qapms.jsx
@@ -0,0 +1,13 @@
+import Req from "./request";
+
+export default function QAPMS() {
+    return (
+      <div>
+        
+        <div>
+          <Req/>
+        </div>
+      </div>
+    );
+  }
+  
